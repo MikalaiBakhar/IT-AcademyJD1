f18abf662004ebbb22949958d98816560aaf737e
MikalaiBakhar <42712718+MikalaiBakhar@users.noreply.github.com>
2018-10-21 20:51:52 +0300

    Add files via upload

---
+++ HomeWork1/SortArrInsertDown.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class SortArrInsertDown {
+
+	public static void main(String[] args) {
+		int y[] = new int[100];
+		for (int i = 0; i < 100; i++) {
+			y[i] = (int) (Math.random() * 100);
+			System.out.println(y[i]);
+		}
+		int counter = 0;
+		for (int i = 0; i < 100; i++) {
+			for (int j = i; j > 0 && y[j - 1] < y[j]; j--) {
+				counter++;
+				int buf = y[j - 1];
+				y[j - 1] = y[j];
+				y[j] = buf;
+			}
+		}
+		System.out.println("=====================================");
+		System.out.println(Arrays.toString(y));
+	}
+}
